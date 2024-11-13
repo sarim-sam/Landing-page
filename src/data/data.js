5944aa1182ce12bfc675d6cb06b3a514e0eba5e9
sarim <sarimslayerali786@gmail.com>
2024-11-13 20:49:44 +0500

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+export const faqs = Array(5).fill(
+    {
+      question: "Eolor sit amet consectetur nim tincidunt orci?",
+      answer: "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches."
+    }
+);
