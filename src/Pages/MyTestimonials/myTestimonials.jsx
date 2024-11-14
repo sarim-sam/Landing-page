c3d57781903b785984f0c85f2ea359b6bcd5e25d
sarim <sarimslayerali786@gmail.com>
2024-11-13 21:09:22 -0800

    changes

--- src/Pages/MyTestimonials/myTestimonials.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyTestimonials = () => {
-  return (
-    <div>My Testimonials</div>
-  )
-}
-
-export default MyTestimonials
--- src/data/data.js
+++
@@ -1,6 +0,0 @@
-export const faqs = Array(5).fill(
-    {
-      question: "Eolor sit amet consectetur nim tincidunt orci?",
-      answer: "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches."
-    }
-);
