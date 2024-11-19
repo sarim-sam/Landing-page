6237dc3b9aaa01d69d851969fd32d40953511390
sarim <sarimslayerali786@gmail.com>
2024-11-19 02:10:36 -0800

    Design etmekte'

--- src/Pages/MyTestimonials/myTestimonials.jsx
+++ src/Pages/MyTestimonials/myTestimonials.jsx
@@ -7,23 +7,34 @@ import { testimonials } from '../../data/data.js';
 // TestimonialCard.
 const TestimonialCard = ({ name, title, image, icon, quote }) => {
   return (
-    <div className="max-w-md p-8 m-6rounded-xl shadow-lg bg-white relative">
+    <div className="group max-w-md p-8 m-6 rounded-[30px] shadow-lg bg-white relative transform transition-transform duration-200 hover:scale-105 hover:bg-[#684fff] hover:text-white active:scale-110">
       <div className="flex items-center gap-4 mb-4 relative">
         <div className="relative">
-          <img src={image} alt={`${name}`} className="w-16 h-16 rounded-full" />
+          <img
+            src={image}
+            alt={`${name}`}
+            className="w-16 h-16 rounded-full border-2 border-[#f8f9ff] transition-colors duration-200 group-hover:border-white"
+          />
           <div className="absolute -top-16 -right-8 w-16 h-16 rounded-full flex items-center justify-center">
             <img src={icon} alt="icon" className="w-14 h-14" />
           </div>
         </div>
         <div>
-          <p className="text-28 poppins-semibold text-black mb-2">{name}</p>
-          <p className="text-16-1 poppins-medium text-black">{title}</p>
+          <p className="text-28 md:text-28md sm:text-28sm xs:text-28sm ls:text-28sm ms:text-28sm poppins-semibold mb-2 transition-colors duration-200 group-hover:text-white">
+            {name}
+          </p>
+          <p className="text-16-1 poppins-medium transition-colors duration-200 group-hover:text-white">
+            {title}
+          </p>
         </div>
       </div>
-      <p className="text-16-2 text-gray-700 poppins-regular">{quote}</p>
+      <p className="text-16-2 text-gray-700 poppins-regular transition-colors duration-200 group-hover:text-white">
+        {quote}
+      </p>
     </div>
   );
 };
+
 
 // Testimonials.
 export const Testimonials = ({ startIndex, direction }) => {
@@ -57,11 +68,11 @@ const MyTestimonials = () => {
   };
   return (
     <div
-      className="relative w-full min-h-[65vh] bg-[#FFFFFF] mx-auto overflow-hidden flex flex-col items-start justify-start px-4 py-8 sm:px-4 md:px-8 xs:px-12 ls:px-12 ms:px-12 mt-14">
+      className="relative w-full min-h-[55vh] bg-[#FFFFFF] overflow-hidden flex flex-col items-start justify-start  mt-14">
     <img
       src="/myGroup.png"
       alt="Background"
-      className="absolute inset-0 left-5 z-0 w-full coverobject- max-h-[580px] hidden sm:hidden md:block"
+      className="absolute inset-0 left-5 z-0 w-full max-h-[580px] hidden sm:hidden md:block"
     />
 
 <div className='2xl:hidden xl:hidden lg:hidden md:hidden sm:block'>
@@ -69,38 +80,39 @@ const MyTestimonials = () => {
   <img src="/decorator-second.png" className='absolute bottom-[-8rem] left-1/2 transform -translate-x-1/2'/>
   </div>
 
-<div className="flex flex-col z-10 lg:flex-row items-center lg:items-start justify-center w-full mx-2 sm:mx-4 mb-8">
+  <div className="flex flex-col lg:flex-row md:flex-col sm:flex-col xs:flex-col ls:flex-col ms:flex-col items-center justify-around w-full mx-2 sm:mx-4 mb-8 z-10 2xl:-mx-32">
   {/* Heading */}
-  <p className="poppins-bold lg:text-left text-center w-full 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl  ms:text-3xl  mb-4 md:mb-6 sm:mb-8 lg:mb-0">
+  <p className="poppins-bold text-center sm:text-left w-full sm:w-auto 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl ms:text-3xl mb-4 sm:mb-0">
     What Our Client Said about us
   </p>
 
   {/* Navigation Buttons */}
-  <div className="flex gap-6 justify-center lg:w-1/2 w-full">
-  {/* LEFT BUTTON */}
+  <div className="flex gap-4 sm:gap-6">
+    {/* LEFT BUTTON */}
     <button
       onClick={handlePrev}
-      className="w-20 h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#f8f9ff] hover:bg-[#e0e4ff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 active:scale-95"
+      className="w-20 h-20 rounded-full bg-[#f8f9ff] hover:bg-[#e0e4ff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 active:scale-95"
     >
       <svg width="86" height="85" viewBox="0 0 86 85" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="0.5" width="85" height="85" rx="42.5" fill="#F8F9FF"/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M55.2498 2.5C55.2498 42.9062 55.0885 43.2956 54.8013 43.5828C54.5142 43.87 54.1247 44.0313 53.7186 44.0313H35.9775L42.5527 50.6034C42.6951 50.7458 42.808 50.9148 42.885 51.1008C42.9621 51.2868 43.0017 51.4862 43.0017 51.6875C43.0017 51.8889 42.9621 52.0883 42.885 52.2743C42.808 52.4603 42.6951 52.6293 42.5527 52.7717C42.4103 52.914 42.2413 53.027 42.0553 53.104C41.8693 53.1811 41.6699 53.2207 41.4686 53.2207C41.2672 53.2207 41.0678 53.1811 40.8818 53.104C40.6958 53.027 40.5268 52.914 40.3844 52.7717L31.1969 43.5842C31.0543 43.4419 30.9412 43.273 30.864 43.0869C30.7868 42.9009 30.7471 42.7015 30.7471 42.5C30.7471 42.2986 30.7868 42.0992 30.864 41.9132C30.9412 41.7271 31.0543 41.5582 31.1969 41.4159L40.3844 32.2284C40.672 31.9409 41.0619 31.7794 41.4686 31.7794C41.8752 31.7794 42.2652 31.9409 42.5527 32.2284C42.8402 32.5159 43.0017 32.9059 43.0017 33.3125C43.0017 33.7192 42.8402 34.1091 42.5527 34.3967L35.9775 40.9688H53.7186C54.1247 40.9688 54.5142 41.1301 54.8013 41.4173C55.0885 41.7045 55.2498 42.0939 55.2498 42.5Z" fill="#684FFF"/>
+        <rect x="0.5" width="85" height="85" rx="42.5" fill="#F8F9FF" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M55.2498 2.5C55.2498 42.9062 55.0885 43.2956 54.8013 43.5828C54.5142 43.87 54.1247 44.0313 53.7186 44.0313H35.9775L42.5527 50.6034C42.6951 50.7458 42.808 50.9148 42.885 51.1008C42.9621 51.2868 43.0017 51.4862 43.0017 51.6875C43.0017 51.8889 42.9621 52.0883 42.885 52.2743C42.808 52.4603 42.6951 52.6293 42.5527 52.7717C42.4103 52.914 42.2413 53.027 42.0553 53.104C41.8693 53.1811 41.6699 53.2207 41.4686 53.2207C41.2672 53.2207 41.0678 53.1811 40.8818 53.104C40.6958 53.027 40.5268 52.914 40.3844 52.7717L31.1969 43.5842C31.0543 43.4419 30.9412 43.273 30.864 43.0869C30.7868 42.9009 30.7471 42.7015 30.7471 42.5C30.7471 42.2986 30.7868 42.0992 30.864 41.9132C30.9412 41.7271 31.0543 41.5582 31.1969 41.4159L40.3844 32.2284C40.672 31.9409 41.0619 31.7794 41.4686 31.7794C41.8752 31.7794 42.2652 31.9409 42.5527 32.2284C42.8402 32.5159 43.0017 32.9059 43.0017 33.3125C43.0017 33.7192 42.8402 34.1091 42.5527 34.3967L35.9775 40.9688H53.7186C54.1247 40.9688 54.5142 41.1301 54.8013 41.4173C55.0885 41.7045 55.2498 42.0939 55.2498 42.5Z" fill="#684FFF" />
       </svg>
     </button>
 
-  {/* RIGHT BUTTON */}
+    {/* RIGHT BUTTON */}
     <button
       onClick={handleNext}
-      className="w-20 h-20 lg:w-20 lg:h-20 md:w-16 md:h-16 sm:w-16 sm:h-16 xs:w-16 xs:h-16 ls:w-16 ls:h-16 ms:w-16 ms:h-16 rounded-full bg-[#684fff] hover:bg-[#836fff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 active:scale-95"
+      className="w-20 h-20 rounded-full bg-[#684fff] hover:bg-[#836fff] flex items-center justify-center transform transition-transform duration-300 hover:scale-110 active:scale-95"
     >
       <svg width="86" height="85" viewBox="0 0 86 85" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="1" y="0.5" width="84" height="84" rx="42" fill="#684FFF"/>
-        <rect x="1" y="0.5" width="84" height="84" rx="42" stroke=""/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M30.7502 42.5C30.7502 42.9062 30.9115 43.2956 31.1987 43.5828C31.4858 43.87 31.8753 44.0313 32.2814 44.0313H50.0225L43.4473 50.6034C43.3049 50.7458 43.192 50.9148 43.115 51.1008C43.0379 51.2868 42.9983 51.4862 42.9983 51.6875C42.9983 51.8889 43.0379 52.0883 43.115 52.2743C43.192 52.4603 43.3049 52.6293 43.4473 52.7717C43.5897 52.914 43.7587 53.027 43.9447 53.104C44.1307 53.1811 44.3301 53.2207 44.5314 53.2207C44.7328 53.2207 44.9322 53.1811 45.1182 53.104C45.3042 53.027 45.4732 52.914 45.6156 52.7717L54.8031 43.5842C54.9457 43.4419 55.0588 43.273 55.136 43.0869C55.2132 42.9009 55.2529 42.7015 55.2529 42.5C55.2529 42.2986 55.2132 42.0992 55.136 41.9132C55.0588 41.7271 54.9457 41.5582 54.8031 41.4159L45.6156 32.2284C45.328 31.9409 44.9381 31.7794 44.5314 31.7794C44.1248 31.7794 43.7348 31.9409 43.4473 32.2284C43.1598 32.5159 42.9983 32.9059 42.9983 33.3125C42.9983 33.7192 43.1598 34.1091 43.4473 34.3967L50.0225 40.9688H32.2814C31.8753 40.9688 31.4858 41.1301 31.1987 41.4173C30.9115 41.7045 30.7502 42.0939 30.7502 42.5Z" fill="white"/>
+        <rect x="1" y="0.5" width="84" height="84" rx="42" fill="#684FFF" />
+        <rect x="1" y="0.5" width="84" height="84" rx="42" stroke="" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M30.7502 42.5C30.7502 42.9062 30.9115 43.2956 31.1987 43.5828C31.4858 43.87 31.8753 44.0313 32.2814 44.0313H50.0225L43.4473 50.6034C43.3049 50.7458 43.192 50.9148 43.115 51.1008C43.0379 51.2868 42.9983 51.4862 42.9983 51.6875C42.9983 51.8889 43.0379 52.0883 43.115 52.2743C43.192 52.4603 43.3049 52.6293 43.4473 52.7717C43.5897 52.914 43.7587 53.027 43.9447 53.104C44.1307 53.1811 44.3301 53.2207 44.5314 53.2207C44.7328 53.2207 44.9322 53.1811 45.1182 53.104C45.3042 53.027 45.4732 52.914 45.6156 52.7717L54.8031 43.5842C54.9457 43.4419 55.0588 43.273 55.136 43.0869C55.2132 42.9009 55.2529 42.7015 55.2529 42.5C55.2529 42.2986 55.2132 42.0992 55.136 41.9132C55.0588 41.7271 54.9457 41.5582 54.8031 41.4159L45.6156 32.2284C45.328 31.9409 44.9381 31.7794 44.5314 31.7794C44.1248 31.7794 43.7348 31.9409 43.4473 32.2284C43.1598 32.5159 42.9983 32.9059 42.9983 33.3125C42.9983 33.7192 43.1598 34.1091 43.4473 34.3967L50.0225 40.9688H32.2814C31.8753 40.9688 31.4858 41.1301 31.1987 41.4173C30.9115 41.7045 30.7502 42.0939 30.7502 42.5Z" fill="white" />
       </svg>
     </button>
   </div>
 </div>
+
 
 
       {/* Testimonials */}
