38ac3ce22281c280d266a42f19dbd1fecfa9c464
sarim <sarimslayerali786@gmail.com>
2024-11-20 03:31:37 -0800

    Desiign changes e'

--- src/Pages/MyTestimonials/myTestimonials.jsx
+++ src/Pages/MyTestimonials/myTestimonials.jsx
@@ -10,7 +10,7 @@ import arrowRight from '@iconify/icons-mdi/arrow-right';
 // TestimonialCard.
 const TestimonialCard = ({ name, title, image, icon, quote }) => {
   return (
-    <div className="group max-w-xl sxl:p-10 sxl:mt-2 lg:p-10 lg:mt-2 rounded-[30px] shadow-lg bg-white relative transform transition-transform duration-300 hover:scale-105 hover:bg-[#684fff] hover:text-white active:scale-110 will-change-transform">
+    <div className="group max-w-md p-8 m-6 rounded-[30px] shadow-lg bg-white relative transform transition-transform duration-300 hover:scale-105 hover:bg-[#684fff] hover:text-white active:scale-110 will-change-transform">
       <div className="flex items-center gap-4 mb-4 relative">
         <div className="relative">
           <img
@@ -18,7 +18,7 @@ const TestimonialCard = ({ name, title, image, icon, quote }) => {
             alt={`${name}`}
             className="w-16 h-16 rounded-full shadow-[0_0_0_2px_#f8f9ff] transition-shadow duration-600 group-hover:shadow-[0_0_0_2px_white]"
           />
-          <div className="absolute -top-16 -right-8 w-16 h-16 rounded-full flex items-center justify-center transition-colors duration-300">
+        <div className="absolute -top-16 -right-8 w-16 h-16 rounded-full flex items-center justify-center transition-colors duration-300">
             <img
               src={icon}
               alt="icon"
@@ -27,10 +27,10 @@ const TestimonialCard = ({ name, title, image, icon, quote }) => {
           </div>
         </div>
         <div>
-          <p className="sxl:text-28 md:text-28md sm:text-28sm xs:text-28sm ls:text-28sm ms:text-28sm poppins-semibold mb-2 transition-colors duration-600 group-hover:text-white">
+          <p className="text-28 md:text-28md sm:text-28sm xs:text-28sm ls:text-28sm ms:text-28sm poppins-semibold mb-2 transition-colors duration-600 group-hover:text-white">
             {name}
           </p>
-          <p className="sxl:text-16-1 poppins-medium transition-colors duration-600 group-hover:text-white">
+          <p className="text-16-1 poppins-medium transition-colors duration-600 group-hover:text-white">
             {title}
           </p>
         </div>
@@ -44,20 +44,15 @@ const TestimonialCard = ({ name, title, image, icon, quote }) => {
 
 
 // Testimonials.
-export const Testimonials = ({ startIndex, direction, animatedIndex }) => {
+export const Testimonials = ({ startIndex, direction }) => {
   const visibleTestimonials = testimonials.slice(startIndex, startIndex + 3);
   return (
-  <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {visibleTestimonials.map((testimonial, index) => {
-        const animationClass =
-          index + startIndex === animatedIndex ? direction : "";
-
-        return (
-          <div key={index} className={`testimonial-card ${animationClass}`}>
-            <TestimonialCard {...testimonial} />
-          </div>
-        );
-      })}
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {visibleTestimonials.map((testimonial, index) => (
+        <div key={index} className={`testimonial-card ${direction}`}>
+          <TestimonialCard {...testimonial} />
+        </div>
+      ))}
     </div>
   );
 };
@@ -65,41 +60,39 @@ export const Testimonials = ({ startIndex, direction, animatedIndex }) => {
 // MyTestimonials.
 const MyTestimonials = () => {
   const [startIndex, setStartIndex] = useState(0);
-  const [animatedIndex, setAnimatedIndex] = useState(null);
   const [direction, setDirection] = useState('');
   const handleNext = () => {
     if (startIndex + 3 < testimonials.length) {
-      setAnimatedIndex(startIndex + 3);
       setDirection('slide-next');
       setStartIndex(startIndex + 1);
-      setTimeout(() => setAnimatedIndex(null), 500);
     }
   };
   const handlePrev = () => {
     if (startIndex > 0) {
-      setAnimatedIndex(startIndex - 1);
       setDirection('slide-prev');
       setStartIndex(startIndex - 1);
-      setTimeout(() => setAnimatedIndex(null), 500);
     }
   };
   return (
     <div
-      className="relative w-full min-h-[637px] bg-[#FFFFFF] overflow-hidden flex flex-col items-start justify-start mt-14">
+      className="relative w-full min-h-[55vh] bg-[#FFFFFF] overflow-hidden flex flex-col items-start justify-start mt-14">
     <img
       src="/myGroup.png"
       alt="Background"
-      className="absolute inset-0 left-5 z-0 max-w-full sm:max-h-[600px] hidden sm:hidden md:block"
+      className="absolute inset-0 left-5 z-0 max-w-full max-h-[580px] hidden sm:hidden md:block"
     />
 
-<div className='sxl:hidden lg:hidden md:hidden sm:block'>
+<div className='2xl:hidden xl:hidden lg:hidden md:hidden sm:block'>
   <img src="/decorator-first.png"  className='absolute left-1/2 transform -translate-x-1/2'/>
   <img src="/decorator-second.png" className='absolute bottom-[-8rem] left-1/2 transform -translate-x-1/2'/>
   </div>
 
-  <div className="flex sxl:h-28 flex-col lg:flex-row md:flex-col sm:flex-col xs:flex-col ls:flex-col ms:flex-col items-center justify-around w-full mx-2 sm:mx-4 mb-8 z-10 2xl:-mx-16">
+  {/* <div className="flex items-center w-full mx-2 sm:mx-4 mb-8 mt-12">
+  <p className="text-5xl sm:text-3xl md:text-5xl poppins-bold mr-auto py-8"> */}
+
+<div className="flex 2xl:h-28 flex-col lg:flex-row md:flex-col sm:flex-col xs:flex-col ls:flex-col ms:flex-col items-center w-full 2xl:mx-2 sm:mx-4 mb-8 z-10">
   {/* Heading */}
-  <p className="poppins-bold text-center sm:text-left w-full sm:w-auto 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl ms:text-3xl mb-4 sm:mb-0">
+  <p className="poppins-bold sxl:px-12 w-full text-center md:text-center lg:text-left 2xl:text-40 xl:text-40 lg:text-40 md:text-3xl sm:text-3xl xs:text-3xl ls:text-3xl ms:text-3xl mb-4 sm:mb-0 py-8">
     What Our Client Said about us
   </p>
 
@@ -118,7 +111,7 @@ const MyTestimonials = () => {
 </div>
 
       {/* Testimonials */}
-      <Testimonials startIndex={startIndex} direction={direction} animatedIndex={animatedIndex} />
+      <Testimonials startIndex={startIndex} direction={direction} />
     </div>
   );
 };
