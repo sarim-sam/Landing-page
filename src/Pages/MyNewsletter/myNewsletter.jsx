f3e9da726a624248e24062d80dfa3b511966b969
sarim <sarimslayerali786@gmail.com>
2024-11-21 00:53:33 -0800

    Changes e'

--- src/Pages/MyNewsletter/myNewsletter.jsx
+++ src/Pages/MyNewsletter/myNewsletter.jsx
@@ -14,11 +14,11 @@ const MyNewsletter = () => {
       />
 
       <div className="relative flex items-center justify-center text-white space-x-8"></div>
-      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40sm xs:text-40sm ls:text-40sm ms:text-40sm z-10 poppins-bold text-center mb-4 text-white min-h-12">
+      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40sm xs:text-40sm ls:text-40sm ms:text-40sm z-10 poppins-bold text-center pb-4 text-white min-h-12">
         Subscribe Newsletter & get
       </p>
       
-      <p className=" 2xl:text-16-5 xl:text-16-5 lg:text-16-5 md:text-16-5md sm:text-16-5sm xs:text16-5sm ls:text16-5sm ms:text16-5sm text-center p-1 pl-2 mb-8 poppins-medium max-w-lg opacity-90 lh-32 text-white">
+      <p className=" 2xl:text-16-5 xl:text-16-5 lg:text-16-5 md:text-16-5md sm:text-16-5sm xs:text16-5sm ls:text16-5sm ms:text16-5sm text-center p-1 pl-2 pb-8 poppins-medium max-w-lg opacity-90 lh-32 text-white">
         If you are going to use a passage of Lorem Ipsum, you need to be sure there isn&apos;t anything embarrassing.
       </p>
 
--- src/Pages/MyWorkflow/myWorkflow.jsx
+++ src/Pages/MyWorkflow/myWorkflow.jsx
@@ -4,13 +4,13 @@ const MyWorkflow = () => {
   return (
     <div className="min-h-[427px] flex flex-col items-center justify-center px-4 bg-white">
     <div className='mt-8'>
-      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40md xs:text-40md ls:text-40md ms:text-40md poppins-bold text-center text-gray-900 mx-8">
+      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40md xs:text-40md ls:text-40md ms:text-40md poppins-bold text-center text-gray-900 px-8">
         Elevate Your Workflow with AI-
       </p>
-      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40md xs:text-40md ls:text-40md ms:text-40md poppins-bold text-center text-gray-900 mb-4 mx-8">Powered Solutions</p>
+      <p className="2xl:text-40 xl:text-40 lg:text-40 md:text-40md sm:text-40md xs:text-40md ls:text-40md ms:text-40md poppins-bold text-center text-gray-900 pb-4 px-8">Powered Solutions</p>
     </div>
       
-      <p className="2xl:text-16-5 xl:text-16-5 md:text-16-5md sm:text-16-5md xs:text16-5md ls:text16-5md ms:text16-5md text-center text-gray-700 font-inter inter-r max-w-4xl mb-12">
+      <p className="2xl:text-16-5 xl:text-16-5 md:text-16-5md sm:text-16-5md xs:text16-5md ls:text16-5md ms:text16-5md text-center text-gray-700 font-inter inter-r max-w-4xl pb-12">
         "Revolutionize your campaigns with seamless AI automation. Manage, optimize, and scale with our
         smart platform that ensures every email reaches its target, increases engagement, and drives results.
         Take control of your campaigns with powerful analytics and insights from one central hub."
