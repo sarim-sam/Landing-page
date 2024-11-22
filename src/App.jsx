712262c99a3d602d35e0a409a212dbed1448d922
sarim <sarimslayerali786@gmail.com>
2024-11-22 02:54:00 -0800

    design sam e'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MyIntegration from './Pages/MyIntegration/myIntegration.jsx';
 import MyQuestions from './Pages/MyQuestions/myQuestions.jsx';
-import MyCarousel from './Pages/MyCarousel/myCarousel.jsx';
 import MyNewsletter from './Pages/MyNewsletter/myNewsletter.jsx';
 import MyWorkflow from './Pages/MyWorkflow/myWorkflow.jsx';
 import MyFooter from './Pages/MyFooter/myFooter.jsx';
